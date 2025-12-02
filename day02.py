2f7d6f06267a44415c3205aad0eb03c680b13f57
DryHop2 <tyler.savon.smith@gmail.com>
2025-12-02 03:00:09 -0700

    day02 part 2

--- day02.py
+++ day02.py
@@ -1,13 +1,34 @@
 PATH = "./inputs/day02.txt"
 
 
+# def is_invalid_pid(n: int) -> bool:
+#     s = str(n)
+#     if len(s) % 2 != 0:
+#         return False
+    
+#     midpoint = len(s) // 2
+#     return s[:midpoint] == s[midpoint:]
+
+
 def is_invalid_pid(n: int) -> bool:
     s = str(n)
-    if len(s) % 2 != 0:
+    L = len(s)
+    if L < 2:
         return False
     
-    midpoint = len(s) // 2
-    return s[:midpoint] == s[midpoint:]
+    for chunk_len in range(1, L // 2 + 1):
+        if L % chunk_len != 0:
+            continue
+
+        repeats = L // chunk_len
+        if repeats < 2:
+            continue
+
+        chunk = s[:chunk_len]
+        if chunk * repeats == s:
+            return True
+    
+    return False
 
 
 def sum_invalid_pids(path: str = PATH) -> int:
