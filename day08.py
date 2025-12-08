f0c15bf08ef2bdb6b9197f2c44279faae8e84dfb
DryHop2 <tyler.savon.smith@gmail.com>
2025-12-08 01:55:11 -0700

    day08 puzzle 2

--- day08.py
+++ day08.py
@@ -13,6 +13,23 @@ def parse(text: str):
     return points
 
 
+def build_edges(points):
+    n = len(points)
+    edges = []
+    for i in range(n):
+        x1, y1, z1 = points[i]
+        for j in range(i + 1, n):
+            x2, y2, z2 = points[j]
+            dx = x1 - x2
+            dy = y1 - y2
+            dz = z1 - z2
+            dist2 = dx*dx + dy*dy + dz*dz
+            edges.append((dist2, i, j))
+    edges.sort(key=lambda e: e[0])
+
+    return edges
+
+
 def part1(text: str, num_pairs: int = 1000):
     points = parse(text)
     n = len(points)
@@ -68,11 +85,62 @@ def part1(text: str, num_pairs: int = 1000):
     return prod(sizes[:3])
 
 
+def part2(text: str):
+    points = parse(text)
+    n = len(points)
+    edges = build_edges(points)
+
+    parent = list(range(n))
+    size = [1] * n
+
+    def find(x):
+        while parent[x] != x:
+            parent[x] = parent[parent[x]]
+            x = parent[x]
+
+        return x
+    
+
+    def union(a, b):
+        ra = find(a)
+        rb = find(b)
+        if ra == rb:
+            return False
+        if size[ra] < size[rb]:
+            ra, rb = rb, ra
+        parent[rb] = ra
+        size[ra] += size[rb]
+
+        return True
+    
+
+    components = n
+    last_pair = None
+    
+
+    for _, i, j in edges:
+        if union(i, j):
+            components -= 1
+            last_pair = (i, j)
+            if components == 1:
+                break
+
+    if last_pair is None:
+        raise RuntimeError("Graph never fully connected")
+    
+    i, j = last_pair
+    x1 = points[i][0]
+    x2 = points[j][0]
+    
+    return x1 * x2
+
+
 def main(path: str = PATH):
     with open(path, "r") as f:
         data = f.read()
 
     print(part1(data))
+    print(part2(data))
 
 
 if __name__ == "__main__":
